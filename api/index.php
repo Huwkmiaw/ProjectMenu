9ae9b2cde2df13f0889b34db51dc3f8ec968980e
Muhamad_Sahal_Ansar <sahalanshar65@gmail.com>
2026-08-28 00:44:32 +0800

    Configure writable bootstrap cache in /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -4,10 +4,36 @@ ini_set('display_errors', '1');
 ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-// Prepare storage directory in /tmp for Vercel serverless environment
+// Prepare writable bootstrap cache directory in /tmp
+$bootstrapCache = '/tmp/bootstrap/cache';
+if (!is_dir($bootstrapCache)) {
+    mkdir($bootstrapCache, 0777, true);
+}
+
+if (file_exists(__DIR__ . '/../bootstrap/cache/packages.php')) {
+    @copy(__DIR__ . '/../bootstrap/cache/packages.php', $bootstrapCache . '/packages.php');
+}
+if (file_exists(__DIR__ . '/../bootstrap/cache/services.php')) {
+    @copy(__DIR__ . '/../bootstrap/cache/services.php', $bootstrapCache . '/services.php');
+}
+
+putenv("APP_SERVICES_CACHE={$bootstrapCache}/services.php");
+putenv("APP_PACKAGES_CACHE={$bootstrapCache}/packages.php");
+putenv("APP_CONFIG_CACHE={$bootstrapCache}/config.php");
+putenv("APP_ROUTES_CACHE={$bootstrapCache}/routes-v7.php");
+putenv("APP_EVENTS_CACHE={$bootstrapCache}/events.php");
+
+$_ENV['APP_SERVICES_CACHE'] = "{$bootstrapCache}/services.php";
+$_ENV['APP_PACKAGES_CACHE'] = "{$bootstrapCache}/packages.php";
+$_ENV['APP_CONFIG_CACHE'] = "{$bootstrapCache}/config.php";
+$_ENV['APP_ROUTES_CACHE'] = "{$bootstrapCache}/routes-v7.php";
+$_ENV['APP_EVENTS_CACHE'] = "{$bootstrapCache}/events.php";
+
+// Prepare writable storage directory in /tmp
 $storagePath = '/tmp/storage';
 $dirs = [
     $storagePath . '/app/public',
+    $storagePath . '/app/private',
     $storagePath . '/framework/views',
     $storagePath . '/framework/cache/data',
     $storagePath . '/framework/sessions',
@@ -21,6 +47,7 @@ foreach ($dirs as $dir) {
 }
 
 putenv("APP_STORAGE={$storagePath}");
+putenv("VIEW_COMPILED_PATH={$storagePath}/framework/views");
 $_ENV['APP_STORAGE'] = $storagePath;
 $_SERVER['APP_STORAGE'] = $storagePath;
 
